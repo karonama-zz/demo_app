43b57fa8ed54bbd282b56fc782db09901b34abed
karo kila <kakila@gopng.gov.pg>
2016-07-21 12:27:49 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '3ffc1cfa8822a675bb99a809087f328eaaaeb8c28d820e4d1f03b042ba4bf5eecdb85d4e2f027b82f0c15701300b3abcc1519447b738030a58018f5da7b20178'
